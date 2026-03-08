4d5410d8add37b3645bd74274500c9561471a2e2
Nash143 <156482623+Nash143@users.noreply.github.com>
2026-03-08 12:41:08 -0400

    Update default event date to July 11, 2026

--- Js/script.js
+++ Js/script.js
@@ -147,7 +147,7 @@ function showCountdown() {
   let startParam = getURLParam('start');
   let eventParam = getURLParam('event');
   let startDate = startParam ? new Date(startParam + 'T00:00:00') : new Date('2024-07-11T00:00:00'); 
-  let eventDate = eventParam ? new Date(eventParam + 'T00:00:00') : new Date('2025-07-11T00:00:00');
+  let eventDate = eventParam ? new Date(eventParam + 'T00:00:00') : new Date('2026-07-11T00:00:00');
 
   function update() {
     const now = new Date();
